f163c157a2aea6a4e00a2c4f40c9597a11a36e24
Janat Taerakul <janat.taerakul@gmail.com>
2023-06-19 13:01:46 +0000

    Remove unused comment

--- src/App.tsx
+++ src/App.tsx
@@ -45,14 +45,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
         <h1
           style={{color: "lightblue"}}
         >
