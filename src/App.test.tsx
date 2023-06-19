8a6013e6e6cceff14c7b39650b7ea7fe32a7a56c
Janat Taerakul <jtaerakul@indeed.com>
2023-06-20 01:03:52 +0900

    Update tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,14 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Window Width', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const windowWidth = screen.getByText(/The width of the window is/i);
+  expect(windowWidth).toBeInTheDocument();
+});
+
+test('Window Height', () => {
+  render(<App />);
+  const windowHeight = screen.getByText(/The height of the window is/i);
+  expect(windowHeight).toBeInTheDocument();
 });
